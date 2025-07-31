926e73e7ae457d9199707846d22fae073e336fe7
Anser-idenbrid <testing006900@gmail.com>
2025-07-31 10:06:18 +0500

    #FIX - landing screen - fixed accordion section

--- src/Components/ui/CustomCollapse.jsx
+++ src/Components/ui/CustomCollapse.jsx
@@ -4,30 +4,31 @@ import { faqData } from '../../Static/FAQData'
 
 const App = () => (
   <Space direction='vertical'>
-   <Space direction='vertical' className='w-full'>
-    {faqData.map(({ key, day, month, label, text }) => (
-      <div key={key} className='relative'>
-        <div className='h-[120px] w-[78px] md:w-[99px] bg-[#5183F4] absolute z-10 rounded-l-[24px] flex flex-col justify-center items-center text-white font-medium text-xl xl:text-3xl'>
-          <div>{day}</div>
-          <div>{month}</div>
-        </div>
+    <Space direction='vertical' className='w-full'>
+      {faqData.map(({ key, day, month, label, text }) => (
+        <div key={key} className='relative'>
 
-        <div className='pl-[78px] md:pl-[99px]'>
-          <Collapse
-            size='large'
-            accordion
-            items={[
-              {
-                key,
-                label,
-                children: <p>{text}</p>,
-              },
-            ]}
-          />
+          <div className=''>
+          <div className='h-[120px] w-[99px] bg-[#5183F4] absolute z-10 rounded-l-[24px] flex flex-col justify-center items-center text-white font-medium text-xl xl:text-3xl'>
+            <div>{day}</div>
+            <div>{month}</div>
+          </div>
+            <Collapse
+            className=''
+              size='large'
+              accordion
+              items={[
+                {
+                  key,
+                  label,
+                  children: <p>{text}</p>,
+                },
+              ]}
+            />
+          </div>
         </div>
-      </div>
-    ))}
-  </Space>
+      ))}
+    </Space>
   </Space>
 )
 export default App
